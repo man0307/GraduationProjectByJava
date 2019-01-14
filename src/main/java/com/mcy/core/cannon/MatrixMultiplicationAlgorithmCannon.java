f2ba87bb936e40b5f723fd85f5a064f1a4b7184a
manchaoyang <1505191091l@qq.com>
2019-01-14 12:46:16 +0800

    创建git仓库

---
+++ src/main/java/com/mcy/core/cannon/MatrixMultiplicationAlgorithmCannon.java
@@ -0,0 +1,13 @@
+package com.mcy.core.cannon;
+
+import java.util.ArrayList;
+
+public class MatrixMultiplicationAlgorithmCannon implements MatrixMultiplicationAlgorithm{
+    public int[][] computeByArray(int[][] matrix1, int[][] matrix2) {
+        return new int[0][];
+    }
+
+    public ArrayList<ArrayList<Integer>> computeByList(ArrayList<ArrayList<Integer>> matrix1, ArrayList<ArrayList<Integer>> matrix2) {
+        return null;
+    }
+}
